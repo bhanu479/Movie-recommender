fd8bc85177375f2de00ee06dbf42431aa3c4b0ad
Bhanu pratap singh shekhawat <51327892+bhanu479@users.noreply.github.com>
2022-01-18 20:49:45 +0530

    Joblib update

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request
-import pickle
 import pandas as pd
 from joblib import load
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 movies_data = pd.read_csv("movies_data.csv")
 movies = pd.read_csv("movies_tags.csv")
 poster = pd.read_csv("poster.csv")
+def create_similarity(data):
+    cv = CountVectorizer(max_features=5000,stop_words='english')
+    vectors = cv.fit_transform(data.tags).toarray()
+    similarity = cosine_similarity(vectors)
+    return similarity
 def recommender(movie):
-    similarity = load('similarity.joblib')
+    similarity = create_similarity(movies)
     movie_index = movies[movies['title']==movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:11]
